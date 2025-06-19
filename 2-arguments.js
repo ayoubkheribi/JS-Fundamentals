4606dc875ffa4a1b3e403061df881ca3e67f0e5a
Ayoub <akhribi627@gmail.com>
2025-06-19 19:14:28 +0100

    java script is amazing

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,9 @@
-let school = 'best';
+let school = 'best school';
 
-if(school.length = 4){
+if(school.length <= 1){
   console.log('No argument');
+} else if(school.length <= 4){
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
