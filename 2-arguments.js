0c9a0030c37492580e2c7d6ac2266fdc6ed41655
Ayoub <akhribi627@gmail.com>
2025-06-19 19:19:57 +0100

    java script is amazing

--- 2-arguments.js
+++ 2-arguments.js
@@ -7,3 +7,23 @@ if(school.length <= 1){
 } else {
   console.log('Arguments found');
 }
+
+let schol = 'best';
+
+if(school.length <= 1){
+  console.log('No argument');
+} else if(school.length <= 4){
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+
+let schl = 'best school';
+
+if(school.length <= 1){
+  console.log('No argument');
+} else if(school.length <= 4){
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
